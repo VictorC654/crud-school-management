8818488b2dd487cdc6519308688337cb7a2d1fb5
VictorC654 <victorcalancea654@gmail.com>
2024-10-13 20:50:48 +0300

    Final adjustment

--- database/migrations/2024_10_12_134930_create_classroom_student_table.php
+++ database/migrations/2024_10_12_134930_create_classroom_student_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('classroom_student', function (Blueprint $table) {
             $table->id();
             $table->foreignId('classroom_id')->constrained()->onDelete('cascade');
-            $table->foreignId('user_id')->constrained();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
